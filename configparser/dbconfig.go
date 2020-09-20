5347a879eba8b49df5e1084df1004a105156f03a
Umang Parmar <umangjparmar@gmail.com>
2020-09-20 15:22:43 +0530

    Add top level Config type to contain multiple database configs

--- configparser/dbconfig.go
+++ configparser/dbconfig.go
@@ -1,9 +1,16 @@
 package configparser
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
-// DBConfig represents a single config object created from config file
-type DBConfig struct {
+// Config is config
+type Config struct {
+	DBList []Database `json:"databases"`
+}
+
+// Database represents a single config object created from config file
+type Database struct {
 	Name      string  `json:"name"`
 	Host      string  `json:"host,omitempty"`
 	Port      uint16  `json:"port,omitempty"`
@@ -12,10 +19,10 @@ type DBConfig struct {
 	Threshold float32 `json:"threshold,omitempty"`
 }
 
-// New returns a new DBConfig object
+// New returns a new Database object
 // in: json formatted string
-func New(conf string) *DBConfig {
-	var dbc DBConfig
+func New(conf string) *Database {
+	var dbc Database
 	err := json.Unmarshal([]byte(conf), &dbc)
 	if err != nil {
 		return nil
