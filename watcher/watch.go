b1b19abfcc06e33ae6321b2815a9fe86f4778978
Umang Parmar <umangjparmar@gmail.com>
2020-09-20 18:01:19 +0530

    watcher doesn't need to be db type aware

--- watcher/watch.go
+++ watcher/watch.go
@@ -7,7 +7,7 @@ import (
 	"github.com/darkLord19/dfon/parser"
 )
 
-func startPostgresWatcher(dbase parser.Database) {
+func startWatcher(dbase *parser.Database) {
 	ticker := time.NewTicker(5 * time.Minute)
 	done := make(chan bool)
 	go func() {
@@ -23,13 +23,8 @@ func startPostgresWatcher(dbase parser.Database) {
 }
 
 // Watch starts go routines to monitor given databases
-func Watch(c parser.Config) {
+func Watch(c *parser.Config) {
 	for _, d := range c.DBList {
-		switch d.Type {
-		case "postgres":
-			startPostgresWatcher(d)
-		case "mysql":
-			startMySQLWatcher(d)
-		}
+		startWatcher(&d)
 	}
 }
